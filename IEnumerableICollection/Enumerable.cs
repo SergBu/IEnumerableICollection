3536a02ba4a8e647dd31444f892b5a04ca49cf09
Sergei <sebudnikov@gmail.com>
2023-08-26 15:00:08 +0300

    Добавьте файлы проекта.

---
+++ IEnumerableICollection/Enumerable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableICollection
+{
+    public class Enumerable : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
